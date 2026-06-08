3544d620e9b1e47ad6d8b805f4e214e9fc78ceaf
HisagaZ <82130717+HisagaZ@users.noreply.github.com>
2026-06-08 12:59:03 +0800

    Add files via upload

--- campaign_landing.js
+++ campaign_landing.js
@@ -39,9 +39,15 @@ const footerContent = `
     <div class="footer-social">
         <h4>Follow Us</h4>
         <div class="social-icons">
-            <a href="#" class="social-icon">IG</a>
-            <a href="#" class="social-icon">FB</a>
-            <a href="#" class="social-icon">IN</a>
+            <a href="https://instagram.com/helloqweenhq" class="social-icon" aria-label="Instagram">
+                <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7.8 2h8.4A5.8 5.8 0 0 1 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8A5.8 5.8 0 0 1 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2Zm-.2 2A3.6 3.6 0 0 0 4 7.6v8.8A3.6 3.6 0 0 0 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6A3.6 3.6 0 0 0 16.4 4H7.6Zm9.65 1.5a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z"/></svg>
+            </a>
+            <a href="#" class="social-icon" aria-label="Facebook">
+                <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M14 8.3V6.8c0-.72.48-.9.82-.9H17V2.1L14 2c-3.34 0-4.1 2.5-4.1 4.1v2.2H7.3v3.95h2.6V22H14v-9.75h3.22l.43-3.95H14Z"/></svg>
+            </a>
+            <a href="#" class="social-icon" aria-label="LinkedIn">
+                <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M6.94 8.98H3V21h3.94V8.98ZM4.97 3A2.28 2.28 0 1 0 5 7.56 2.28 2.28 0 0 0 4.97 3Zm16.02 11.12c0-3.23-1.72-5.31-4.55-5.31-2.09 0-3.03 1.15-3.55 1.96V8.98H9.12V21h3.93v-5.95c0-1.57.3-3.09 2.24-3.09 1.91 0 1.94 1.79 1.94 3.19V21h3.76v-6.88Z"/></svg>
+            </a>
         </div>
     </div>
 </div>
